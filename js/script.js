a9de9f80945e5afae0b9fd93e0b387865a6338ce
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-09-18 21:22:03 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-function multiply(sayHi) {
-  // Тіло функції
-  console.log('Hello, this is my first function!');
+function checkStorage(available, ordered) {
+  let message;
+  // Change code below this line
+  if (ordered === 0) {
+    message = 'There are no products in the order!';
+  } else if (ordered > available) {
+    message = 'Your order is too large, there are not enough items in stock!';
+  } else {
+    message = 'The order is accepted, our manager will contact you';
+  }
+  // Change code above this line
+  return message;
 }
-
-// 2. Виклики функції multiply
-multiply();
