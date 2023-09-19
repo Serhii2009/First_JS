02b77e83df901a5241588ee789e6e427ed7fb58e
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-09-19 22:38:03 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
-function checkStorage(available, ordered) {
-  let message;
-  // Change code below this line
-  if (ordered === 0) {
-    message = 'There are no products in the order!';
-  } else if (ordered > available) {
-    message = 'Your order is too large, there are not enough items in stock!';
-  } else {
-    message = 'The order is accepted, our manager will contact you';
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+
+  // Змінити код нижче
+  if (totalSpent >= 50000) {
+    discount = 0.1;
+  } else if (totalSpent >= 20000 && totalSpent <= 50000) {
+    discount = 0.05;
+  } else if (totalSpent >= 5000 && totalSpent <= 20000) {
+    discount = 0.02;
+  } else if (totalSpent <= 5000) {
+    discount = 0;
   }
-  // Change code above this line
-  return message;
+  // Змінити код вище
+  return discount;
 }
+
+getDiscount(137000);
+getDiscount(46900);
+getDiscount(8250);
+getDiscount(1300);
+getDiscount(5000);
+getDiscount(20000);
+getDiscount(50000);
