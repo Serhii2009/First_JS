12262173097f2c53fecb7dd0164039ef6c0db55d
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-09-30 00:32:17 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,38 +1,69 @@
-//let order = [34, 50, 3, 4, 5];
-//console.log(calculateTotalPrice(order));
+// ----- Example 1 -------
 
-//function calculateTotalPrice(order) {
-//let total = 0;
-//for (let i = 0; i < order.length; i++) {
-//total += order[i];
+// let order = [34, 50, 3, 4, 5];
+// console.log(calculateTotalPrice(order));
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (let i = 0; i < order.length; i++) {
+//     total += order[i];
+//   }
+//   return total;
 // }
-//return total;
-//}
 
-//let firstArray = ['Artem', 'Serhii'];
-//let secondArray = ['Dima', 'Lorak'];
-//let maxLength = 3;
-//console.log(makeArray(firstArray, secondArray, maxLength));
+// ----- Example 2 -------
 
-//function makeArray(firstArray, secondArray, maxLength) {
-//let newArray = firstArray.concat(secondArray);
-//if (newArray.length > maxLength) {
-//newArray = newArray.slice(0, maxLength);
-//}
-//return newArray;
-//}
+// let firstArray = ['Artem', 'Serhii'];
+// let secondArray = ['Dima', 'Lorak'];
+// let maxLength = 3;
+// console.log(makeArray(firstArray, secondArray, maxLength));
 
-let min = 50;
-let max = 99;
-console.log(createArrayOfNumbers(min, max));
+// function makeArray(firstArray, secondArray, maxLength) {
+//   let newArray = firstArray.concat(secondArray);
+//   if (newArray.length > maxLength) {
+//     newArray = newArray.slice(0, maxLength);
+//   }
+//   return newArray;
+// }
 
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-  // Change code below this line
-  for (let i = min; i <= max; i++) {
-    numbers.push(i);
-  }
+// ----- Example 3 -------
 
-  // Change code above this line
-  return numbers;
-}
+// let min = 50;
+// let max = 99;
+// console.log(createArrayOfNumbers(min, max));
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max; i++) {
+//     numbers.push(i);
+//   }
+
+//   return numbers;
+// }
+
+// ----- Example 4 -------
+
+// function getSum() {
+//   let sum = 0;
+//   const arr = Array.from(arguments);
+//   console.log(arr.slice(2, 4));
+// }
+
+// const result = getSum(2, 3, 4, 5, 6);
+// console.log(result);
+
+// ----- Example 5 -------
+
+// function getSum(...args) {
+//   let sum = 0;
+//   for (const item of args) {
+//     sum += item;
+//   }
+//   return sum;
+// }
+
+// const result = getSum(2, 3, 4, 5, 6);
+// console.log(result);
+
+// ----- Example 6 -------
