2c99b4227c585fdc910cd42618f53fe54b10a947
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-09-29 16:14:26 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-let order = [34, 50, 3, 4, 5];
-console.log(calculateTotalPrice(order));
+//let order = [34, 50, 3, 4, 5];
+//console.log(calculateTotalPrice(order));
 
-function calculateTotalPrice(order) {
-  let total = 0;
-  // Change code below this line
-  for (let i = 0; i < order.length; i++) {
-    total += order[i];
+//function calculateTotalPrice(order) {
+//let total = 0;
+//for (let i = 0; i < order.length; i++) {
+//total += order[i];
+// }
+//return total;
+//}
+
+let firstArray = ['Artem', 'Serhii'];
+let secondArray = ['Dima', 'Lorak'];
+let maxLength = 3;
+console.log(makeArray(firstArray, secondArray, maxLength));
+
+function makeArray(firstArray, secondArray, maxLength) {
+  let newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
+    newArray = newArray.slice(0, maxLength);
   }
-
-  // Change code above this line
-  return total;
+  return newArray;
 }
