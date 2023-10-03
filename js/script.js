64e7eb38ca62892be063176e73458e91e4c5bdfa
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-03 13:46:05 +0300

    new

--- js/script.js
+++ js/script.js
@@ -125,16 +125,190 @@
 
 // ----- Example 9 -------
 
-const words = ['apple', 'banana', 'cherry', 'date'];
-const searchString = 'banana';
+// const words = ['apple', 'banana', 'cherry', 'date'];
+// const searchString = 'banana';
 
-function includesWord(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return true;
-    }
-  }
-  return false;
-}
+// function includesWord(array, value) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-console.log(includesWord(words, searchString)); // Виведе true, оскільки рядок "banana" є у масиві.
+// console.log(includesWord(words, searchString)); // Виведе true, оскільки рядок "banana" є у масиві.
+
+// ----- Example 10 -------
+
+// ✅ Логічно і синтаксично згруповані сутності
+
+// const bookShelf = {
+//   books: ['The Last Kingdom', 'Dream Guardian'],
+//   // Це метод об'єкта
+//   getBooks() {
+//     console.log('Цей метод буде повертати всі книги - властивість books');
+//   },
+//   // Це метод об'єкта
+//   addBook(bookName) {
+//     console.log('Цей метод буде додавати нову книгу у властивість books');
+//   },
+// };
+
+// // Виклики методів
+// bookShelf.getBooks();
+// bookShelf.addBook('Нова книга');
+
+// ----- Example 11 -------
+
+// const bookShelf = {
+//   books: ['The Last Kingdom'],
+//   getBooks() {
+//     return this.books;
+//   },
+//   addBook(bookName) {
+//     this.books.push(bookName);
+//   },
+//   removeBook(bookName) {
+//     const bookIndex = this.books.indexOf(bookName);
+//     this.books.splice(bookIndex, 1);
+//   },
+// };
+
+// console.log(bookShelf.getBooks()); // ["The Last Kingdom"]
+// bookShelf.addBook('The Mist');
+// bookShelf.addBook('Dream Guardian');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'The Mist', 'Dream Guardian']
+// bookShelf.removeBook('The Mist');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'Dream Guardian']
+
+// ----- Example 12 -------
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(book[key]);
+// }
+
+// ----- Example 13 -------
+
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = 'Манго';
+
+// console.log(dog); // {name: 'Манго'}
+// console.log(dog.name); // 'Манго'
+// console.log(dog.legs); // 4
+
+// Метод Object.create(animal) створює і повертає новий об'єкт, зв'язуючи його з об'єктом animal.
+// Тому можна отримати значення властивості legs, звернувшись до нього як dog.legs,
+// хоча він відсутній в об'єкті dog - це невласна властивість з об'єкта animal.
+
+// ----- Example 14 -------
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Якщо це власна властивість - виконуємо тіло if
+//   if (book.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(book[key]);
+//   }
+
+//   // Якщо це невласна властивість - нічого не робимо
+// }
+
+// ----- Example 15 -------
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+// Вбудований клас Object має декілька корисних методів для роботи з об'єктами.
+// Перший з них - це Object.keys(obj), який приймає об'єкт
+// і повертає масив ключів його власних властивостей.
+// Якщо об'єкт не має властивостей, метод поверне порожній масив.
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+// ----- Example 16 -------
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+// Or🧐
+
+// const goods = {
+//   apples: 6,
+//   grapes: 3,
+//   bread: 4,
+//   cheese: 7,
+// };
+
+// const values = Object.values(goods); // [6, 3, 4, 7]
+
+// let total = 0;
+
+// for (const value of values) {
+//   total += value;
+// }
+
+// console.log(total); // 20
+
+// ----- Example 17 -------
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+// const entries = Object.entries(book);
+// console.log(entries);
+// // [["title", "The Last Kingdom"], ["author", "Bernard Cornwell"], ["rating", 8.38]]
+
+// Метод Object.entries(obj) повертає масив записів, кожен елемент якого,
+//   буде ще один масив з 2 - х елементів:
+// імені властивості і значення цієї властивості з об'єкта obj.
+
+// ----- Example 18 -------
