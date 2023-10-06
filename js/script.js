c332e7ee39cdfa3121ce8fdcfc873b8a18fd954f
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-06 16:44:12 +0300

    new

--- js/script.js
+++ js/script.js
@@ -312,3 +312,26 @@
 // імені властивості і значення цієї властивості з об'єкта obj.
 
 // ----- Example 18 -------
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15, propD: 20 };
+
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15, propD: 20 }
+
+// const fourth = { ...second, ...first };
+// console.log(fourth); // { propA: 5, propB: 10, propC: 50, propD: 20 }
+
+// ----- Example 19 -------
+
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значення першого аргументу
+//   console.log(secondNumber); // Значення другого аргументу
+//   console.log(otherArgs); // Масив інших аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+// ----- Example 20 -------
