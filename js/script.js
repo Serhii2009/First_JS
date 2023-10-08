35aee3dc044b14405082d9ad2cf77024d03b092f
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-08 16:35:36 +0300

    New code

--- js/script.js
+++ js/script.js
@@ -446,3 +446,57 @@
 // }
 
 // ----- Example 27 -------
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// ----- Example 28 -------
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+
+const {
+  today: {
+    high: highToday,
+    low: lowToday,
+    icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+  tomorrow: {
+    high: highTomorrow,
+    low: lowTomorrow,
+    icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+} = forecast;
+
+console.log(highToday);
+console.log(lowToday);
+console.log(todayIcon);
+console.log(highTomorrow);
+console.log(lowTomorrow);
+console.log(tomorrowIcon);
