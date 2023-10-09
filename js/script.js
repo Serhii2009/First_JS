bb1300f56a765eab5de88b49c133982d03d97ca2
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-10 00:00:46 +0300

    Новий код: автоперевірки з 34 завдання

--- js/script.js
+++ js/script.js
@@ -564,3 +564,67 @@
 // console.log(makeTask({ category: 'Finance', text: 'Take interest' }));
 // console.log(makeTask({ priority: 'Low', text: 'Choose shampoo' }));
 // console.log(makeTask({ text: 'Buy bread' }));
+
+// Розібрати завдання знизу, вони з автопровірки!!!!!!
+
+// ----- Example 32 -------
+
+// function findMatches(arr, ...args) {
+//   const matches = [];
+
+//   for (const arg of args) {
+//     if (arr.includes(arg)) {
+//       matches.push(arg);
+//     }
+//   }
+
+//   return matches;
+// }
+
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+// console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+// console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41));
+// console.log(findMatches([63, 11, 8, 29], 4, 7, 16));
+
+// ----- Example 33 -------
+
+// const bookShelf = {
+//   // Change code below this line
+//   books: ['The last kingdom', 'The guardian of dreams'],
+//   getBooks() {
+//     return 'Returning all books';
+//   },
+//   addBook(bookName) {
+//     return `Adding book ${bookName}`;
+//   },
+//   removeBook(bookName) {
+//     return `Deleting book ${bookName}`;
+//   },
+//   updateBook(oldName, newName) {
+//     return `Updating book ${oldName} to ${newName}`;
+//   },
+//   // Change code above this line
+// };
+
+// ----- Example 34 -------
+
+// const bookShelf = {
+//   books: ['The last kingdom', 'Haze', 'The guardian of dreams'],
+//   updateBook(oldName, newName) {
+//     const bookIndex = this.books.indexOf(oldName);
+//     if (bookIndex !== -1) {
+//       this.books[bookIndex] = newName;
+//     }
+//   },
+// };
+
+// ----- Example 35 -------
+
+// const atTheOldToad = {
+//   potions: [],
+// };
+
+// // Перевірка результату
+// console.log(atTheOldToad.potions); // Виведе []
+
+// ----- Example 36 -------
