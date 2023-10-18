711e6da7700b79eb4ccc35b1ed724b177d40399e
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-18 19:27:09 +0300

    Нове!

--- js/script.js
+++ js/script.js
@@ -2664,4 +2664,76 @@
 // // this in foo: {username: 'Resort hotel', showThis: ƒ}
 // // this in showThis: {username: 'Resort hotel',showThis: ƒ}
 
-// 7 ----- Правила визначення ⚜️this⚜️ -------
+// 7 ----- ⚜️Метод call()⚜️ -------
+
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
+
+// const mango = {
+//   username: 'Манго',
+// };
+// const poly = {
+//   username: 'Полі',
+// };
+
+// greetGuest.call(mango, 'Ласкаво просимо'); // Ласкаво просимо, Манго.
+// greetGuest.call(poly, 'З прибуттям'); // З прибуттям, Полі.
+
+// 8 ----- ⚜️Метод apply()⚜️ -------
+
+// function greetGuest(greeting) {
+//   console.log(`${greeting}, ${this.username}.`);
+// }
+
+// const mango = {
+//   username: 'Манго',
+// };
+// const poly = {
+//   username: 'Полі',
+// };
+
+// greetGuest.apply(mango, ['Ласкаво просимо']); // Ласкаво просимо, Манго.
+// greetGuest.apply(poly, ['З прибуттям']); // З прибуттям, Полі.
+
+// 9 ----- ⚜️Метод bind()⚜️ -------
+
+// function greet(clientName) {
+//   return `${clientName}, ласкаво просимо в «${this.service}».`;
+// }
+
+// const steam = {
+//   service: 'Steam',
+// };
+// const steamGreeter = greet.bind(steam);
+// console.log(steamGreeter('Манго')); // "Манго, ласкаво просимо в «Steam»."
+
+// const gmail = {
+//   service: 'Gmail',
+// };
+// const gmailGreeter = greet.bind(gmail);
+// console.log(gmailGreeter('Полі')); // "Полі, ласкаво просимо в «Gmail»."
+
+// 10 ----- ⚜️bind() і методи об'єкта⚜️ -------
+
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() - це виклик методу getFullName без об'єкта
+//   console.log(`Обробляємо заявку від ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName.bind(customer));
+// // makeMessage(customer.getFullName); // Виникне помилка на момент виклику функції
+
+// // // ❌ Було
+// // makeMessage(customer.getFullName); // Виникне помилка на момент виклику функції
+
+// // // ✅ Стало
+// // makeMessage(customer.getFullName.bind(customer)); // Обробляємо заявку від Jacob Mercer.
