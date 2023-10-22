83e4c79ed59f7dc002fd0708ea400e44cc475b0e
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-22 17:32:56 +0300

    Нові автоперевірки 5 модуль

--- js/script.js
+++ js/script.js
@@ -2819,3 +2819,101 @@
 // console.log(dogKeys); // ['barks']
 
 // 17 ----- 🎀Класи🎗️ -------
+
+// -
+// -
+// -
+// -
+// -
+// АВТОПЕРЕВІРКА(5 МОДУЛЬ)
+// -
+// -
+// -
+// -
+// -
+
+// ----- Example 1 -------
+
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
+
+// console.log(pizzaPalace.order('Smoked'));
+// console.log(pizzaPalace.order('Margarita'));
+// console.log(pizzaPalace.order('Four meats'));
+// console.log(pizzaPalace.order('Paperoni'));
+
+// ----- Example 2 -------
+
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// ----- Example 3 -------
+
+// const historyService = {
+//   orders: [
+//     { email: 'jacob@hotmail.com', dish: 'Burrito' },
+//     { email: 'solomon@topmail.net', dish: 'Burger' },
+//     { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//     { email: 'solomon@topmail.net', dish: 'Apple pie' },
+//     { email: 'jacob@hotmail.com', dish: 'Taco' },
+//   ],
+
+//   getOrdersLog() {
+//     return this.orders
+//       .map(order => `email: ${order.email} dish: ${order.dish}`)
+//       .join(' - ');
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+// };
+
+// console.log(historyService.getOrdersByEmail('artemis@coldmail.net'));
+// console.log(historyService.getOrdersByEmail('roma@kendel.net'));
+// console.log(historyService.getOrdersByEmail('solomon@topmail.net'));
+// console.log(historyService.getOrdersByEmail('jacob@hotmail.com'));
