d7b51b877f59be35fc8cad59328ac866c5ee9f82
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-29 18:00:05 +0200

    Новий конспект

--- js/script.js
+++ js/script.js
@@ -3454,3 +3454,25 @@
 //     </ul>
 //   </body>
 // </html>
+
+// ------ 🆗Пошук елементів🤯 -------
+
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.menu');
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll('li');
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll('.menu-item');
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector('.menu-item');
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+
+// ------ 🤡Властивості та атрибути🐲 -------
