0e0b5a3fa1f3c2289bdc5a4e72ba614b2a49872c
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-10-30 18:22:49 +0200

    Додав трохи конспекту

--- js/script.js
+++ js/script.js
@@ -3490,3 +3490,16 @@
 
 // const closeBtn = document.querySelector('button[data-action="close"]');
 // console.log(closeBtn.dataset.action); // "close"
+
+// ------ 🎀Створення елементів✅ ------
+
+// const heading = document.createElement('h1');
+// console.log(heading); // <h1></h1>
+
+// heading.textContent = 'This is a heading';
+// console.log(heading); // <h1>This is a heading</h1>
+
+// const image = document.createElement('img');
+// image.src = 'https://placeimg.com/640/480/nature';
+// image.alt = 'Nature';
+// console.log(image); // <img src="https://placeimg.com/640/480/nature" alt="Nature" />
