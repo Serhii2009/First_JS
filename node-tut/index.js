e855452262ee69c2a09ae969580e665136532bc3
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-15 18:44:16 +0200

    Add node.js

---
+++ node-tut/index.js
@@ -0,0 +1,22 @@
+// Example 1
+
+// const message = 'NodeJS in amazing!';
+// console.log(message);
+
+// Example 2
+
+const validator = require('validator');
+
+const validateEmail = email => {
+  return validator.isEmail(email);
+};
+
+console.log(
+  'Is mango@mail.com a valid email?: ',
+  validateEmail('mango@mail.com')
+);
+
+console.log(
+  'Is Mangozedog.com a valid email?: ',
+  validateEmail('Mangozedog.com')
+);
