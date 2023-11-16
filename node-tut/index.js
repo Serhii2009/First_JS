aa02a2d82b06356901881d19f0b747c1fdd03b09
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-16 16:26:21 +0200

    new

--- node-tut/index.js
+++ node-tut/index.js
@@ -5,18 +5,18 @@
 
 // Example 2
 
-const validator = require('validator');
+// const validator = require('validator');
 
-const validateEmail = email => {
-  return validator.isEmail(email);
-};
+// const validateEmail = email => {
+//   return validator.isEmail(email);
+// };
 
-console.log(
-  'Is mango@mail.com a valid email?: ',
-  validateEmail('mango@mail.com')
-);
+// console.log(
+//   'Is mango@mail.com a valid email?: ',
+//   validateEmail('mango@mail.com')
+// );
 
-console.log(
-  'Is Mangozedog.com a valid email?: ',
-  validateEmail('Mangozedog.com')
-);
+// console.log(
+//   'Is Mangozedog.com a valid email?: ',
+//   validateEmail('Mangozedog.com')
+// );
